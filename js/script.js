f4f79a9e9332f21de3a48e79a4ca31b02512fc81
Vladimir <imagen20071@gmail.com>
2022-03-29 20:16:44 +0300

    release

---
+++ js/script.js
@@ -0,0 +1,32 @@
+require('es6-promise').polyfill();
+import 'nodelist-foreach-polyfill';
+
+import tabs from'./modules/tabs';
+import modal from'./modules/modal';
+import calc from'./modules/calc';
+import slider from'./modules/slider';
+import forms from'./modules/forms';
+import timer from'./modules/timer';
+import cards from'./modules/cards';
+import {openModal} from './modules/modal';
+
+window.addEventListener('DOMContentLoaded', () => {
+    const modalTimerId = setTimeout(() => openModal('.modal', modalTimerId),  300000);
+
+    tabs('.tabheader__item', '.tabcontent', '.tabheader__items', 'tabheader__item_active');
+    modal('[data-modal]', '.modal', modalTimerId);
+    timer('.timer', '2022-05-22');
+    cards();
+    calc();
+    forms('form', modalTimerId);
+    slider({
+        container: '.offer__slider',
+        nextArrow: '.offer__slider-next',
+        prevArrow: '.offer__slider-prev',
+        slide: '.offer__slide',
+        totalCounter: '#total',
+        currentCounter: '#current',
+        wrapper: '.offer__slider-wrapper',
+        field: '.offer__slider-inner'
+    });
+});
